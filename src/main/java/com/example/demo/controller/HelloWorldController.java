41ddc4c23ad45ba212e1c035169d410654360d95
zhouwq <173484695@qq.com>
2018-09-10 20:49:21 +0800

    @value注解使用，推荐使用application.yml文件，而不使用application.properties文件

--- src/main/java/com/example/demo/controller/HelloWorldController.java
+++ src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -1,14 +1,24 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 	
+	@Value("${cupSize}")
+	private String cupSize;
+	
+	@Value("${age}")
+	private int age;
+	
+	@Value("${content}")
+	private String content;
+	
 	@RequestMapping("/hello")
     public String index() {
 		System.out.println("1111111111111111111111111111");
-        return "Hello World";
+        return content;
     }
 }
