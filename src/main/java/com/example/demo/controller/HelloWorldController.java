fc7e45cf35faa3b9815c8cd34d7e565c6eea59d2
zhouwq <173484695@qq.com>
2018-09-10 22:56:01 +0800

    模板thymeleaf使用，以及双映射路径的配置，操作文档说明

--- src/main/java/com/example/demo/controller/HelloWorldController.java
+++ src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,9 +21,10 @@ public class HelloWorldController {
 	@Autowired
 	private GrilProperties grilProperties;
 	
-	@RequestMapping("/hello")
+	@RequestMapping(value= {"/hello","/hi"},method=RequestMethod.GET)
     public String index() {
 		System.out.println("1111111111111111111111111111");
-        return grilProperties.getCupSize()+"=="+grilProperties.getAge();
+		return grilProperties.getCupSize();
+		//return "index";
     }
 }
